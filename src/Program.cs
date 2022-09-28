7b9837690ba6ce1b404108cdbac8ff2e55b7bf7a
BobbyShmurner <glitched.ohdeymus@gmail.com>
2022-09-28 17:38:34 +0100

    Removed Bin And Obj Folders From Repo

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,6 @@
 ﻿class Program {
 	static void Main() {
-		Node node = Node.CreateTreeFromString("(A + C)(AB + A!B) + !B(!B + C) + ((B + C + D) + A + A + C + (B) + B + (A)(B) + C + A + X + F + D + A + A + A + A + A + A)");
+		Node node = Node.CreateTreeFromString("(A + C)(AB + A!B) + !B(!B + C) + !!B!A + ((B + C + D) + A + A + C + (B) + B + (A)(B) + C + A + X + F + D + A + A + A + A + A + A)");
 		// Node node = Node.CreateTreeFromString("C!B + A + B + !AB!C");
 
 		Console.WriteLine($"Node: {node}");
